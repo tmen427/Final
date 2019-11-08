84982fc8b08280cc997d4d2a306963cf0fa5e2ae
tmonnie <tmonnie@tmonnies-MacBook-Air.local>
2019-11-08 12:15:12 -0600

    updates

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,48 +1,41 @@
 import React from "react";
 import "./style.css";
 
-
 function Nav() {
   return (
-
     <div>
-      <header class="main-header dark-header fs-header sticky">
-        <div class="header-inner">
-        
-           
-            
-   
-          
+    <header class="main-header dark-header fs-header sticky">
+      <div class="header-inner">
+       <div class="nav-holder main-menu">
+            <nav >
+              <ul>
 
-            <div class="nav-holder main-menu">
-              <nav >
-                <ul>
+                <li>
+                  <a href="/home">Home <span className="sr-only">(current)</span></a>
 
-                  <li>
-                    <a href="/home">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li>
+                  <a class="act-link" href="/saved">Recent Searches</a>
+                </li>
 
-                  </li>
-                  <li>
-                    <a class="act-link" href="/saved">Recent Searches</a>
-                  </li>
+                <li>
+                  <a href="map.html">Map</a>
 
-                  <li>
-                    <a href="map.html">Map</a>
+                </li>
 
-                  </li>
+                <li> <a href="/inputdata" >User Posts </a></li>
+                <li>
+                <a href="/login" >Sign In</a>   
+                </li>
+               
 
-                  <li> <a href="/inputdata" >User Posts </a></li>
-                  <li>
-                  <a href="/login" >Sign In</a>   
-                  </li>
-                 
-
-                </ul>
-              </nav>
-            </div>
+              </ul>
+            </nav>
           </div>
-    </header>
+        </div>
+  </header>
 </div>
+
 
 
       );
